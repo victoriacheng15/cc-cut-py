07ff831f305433a324c8cb43f7c3ab6c5ed6e3e8
Victoria Cheng <victoria.cheng15@gmail.com>
2025-02-09 12:31:04 -0700

    complete step 4

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import argparse
 
 
@@ -15,19 +16,11 @@ def parse_fields(field_input):
   fields = normalized_input.split(",")
   return [validate_field(field) for field in fields]
 
-def cut(filename, delimiter, fields):
-  try:
-    with open(filename, 'r') as f:
-      for line in f:
-        parts = line.strip().split(delimiter)
-
-        selected_fields = [parts[i-1] for i in fields if i - 1 < len(parts)]
-
-        print(delimiter.join(selected_fields))
-  except FileNotFoundError:
-    print(f'Error: File "{filename}" not found.')
-  except Exception as e:
-    print(f'Error: {e}')
+def cut(input_stream, delimiter, fields):
+  for line in input_stream:
+    parts = line.strip().split(delimiter)
+    selected_fields = [parts[i - 1] for i in fields if i-1 < len(parts)]
+    print(delimiter.join(selected_fields))
 
 
 def main():
@@ -35,14 +28,18 @@ def main():
     parser = argparse.ArgumentParser(description="A Python implementation of the cut command.")
 
     # add arguments
-    parser.add_argument('filename', type=str, help="The file to process.")
+    parser.add_argument('filename', type=str, nargs='?', help="The file to process.")
     parser.add_argument('-d', '--delimiter', type=str, default='\t', help="Field delimiter (default is tab).")
     parser.add_argument('-f', '--fields', type=parse_fields, required=True, help="Fields to select (e.g., 1,2,3).")
 
     # parse arguments
     args = parser.parse_args()
 
-    cut(args.filename, args.delimiter, args.fields)
+    if args.filename:
+      with open(args.filename, 'r') as file:
+        cut(file, args.delimiter, args.fields)
+    else:
+      cut(sys.stdin, args.delimiter, args.fields)
 
 if __name__ == "__main__":
     main()
